2e8a12f55f85ee86c46dff6e588cf6afb138cb44
Tyronne Jeong <maurois@nate.com>
2021-12-12 11:03:33 +0900

    Initial commit

---
+++ param.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Positional Prameters 사용 예제
+
+echo "The first parameters  is : $0"
+echo "The second parameters is : $1"
+echo "The third parameters  is : $2"
+echo "The fourth parameters is : $3"
+echo "The fifth parameters  is : $4"
